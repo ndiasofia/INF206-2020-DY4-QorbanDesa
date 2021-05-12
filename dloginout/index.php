832d7dbd0cc9451beda4a601de09729e6b859c58
Nadia Sofia <fia06@mhs.unsyiah.ac.id>
2021-05-13 00:04:00 +0700

    Menyelesaikan modal di halaman verifikasi, membuat menu daftar akun, login, logout, dan halaman page setelah login

---
+++ dloginout/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Nadia Sofia</title>
+  </head>
+  <body>
+
+    <style>
+      body {
+        background-color: #ffbb0098;
+      }
+    </style>
+    
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-warning bg-warning">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bolder text-muted"><img class="rounded-image" src="img1.jpg" alt="" width="30" height="30" >Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                  <a class="nav-link active fw-bolder text-muted aria-current=page" href="transaksi.html">Home</a>
+                  <a class="nav-link fw-bolder text-muted" href="laporan.html">Hukum Qurban</a>
+                  <a class="nav-link fw-bolder text-muted" href="penerima.html">Tentang Kami</a>
+                  <a class="nav-link fw-bolder text-muted" href="verifikasi.html">List Pengqurban</a>
+                  <a class="nav-link fw-bolder text-muted" href="Verifikasi">List Penerima Qurban</a>
+                  <a class="nav-link fw-bolder text-muted" href="Pesan">Daftar Sebagai Pequrban</a>
+                  <a class="nav-link fw-bolder text-muted" href="Logout">Login</a>
+                  <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+              </div>
+            </div>
+        </div>
+      </nav>
+      <hr color="#999999" size="10px">
+    
+      <h2 class="text-center">~ Selamat Datang ~</h2>
+      <div align="center">
+      <br>
+        <table style="width: 500px; text-align:center" >
+            <tr>
+                <td><a href="register.php" class="btn btn-warning">Daftar</a></td>
+                <td><a href="login.php" class="btn btn-warning">Masuk</a></td>
+            </tr>
+        </table>
+        </div>
+    
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> -->
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    
+    
+  </body>
+</html>
